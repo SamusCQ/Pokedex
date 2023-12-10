2f983c59758192ded3adcd325f3a248ba43d10d8
Samuel Cedeño Quinde <Trollmex_CQ@hotmail.com>
2023-12-10 00:25:37 -0500

    Creacion de paginas

---
+++ src/pages/Pokemons.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+import Header from "../components/Header";
+
+const Pokemons = () => {
+    return (
+        <>
+            <Header />
+            <main>
+                <h1>POKEMONS</h1>
+            </main>
+            <footer>FOOTER</footer>
+        </>
+    );
+};
+
+export default Pokemons;
